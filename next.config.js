00b35f57117f0e1d0b77700289ae3ea5dd98eae8
hoontae24 <kadr8941@gmail.com>
2021-08-11 22:41:59 +0900

    "/" 페이지 리다이렉트 제거

--- next.config.js
+++ next.config.js
@@ -1,11 +1,1 @@
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/1",
-        permanent: true,
-      },
-    ]
-  },
-}
+module.exports = {}
